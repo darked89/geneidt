5d69f3dbb5e66e1f47d717f2046c5b2212f789cd
Darek Kedra <darked89@yahoo.com>
2016-05-03 19:48:39 +0200

    mini temp change of run_test_train.sh: back to test_data/pmarinus4training.gff

--- run_test_train.sh
+++ run_test_train.sh
@@ -5,5 +5,5 @@ echo "running: run_test_train.sh with pmarinus4training data";
 ./gff2geneid_gff.pl -gff test_data/pmarinus4training.gff
 
 
-./geneid_trainer.pl -species pmar01 -gff test_data/pmarinus4training.DOTS.gff.geneid.gff_nodots_sorted -fasta test_data/pmarinus4training.fa \
+./geneid_trainer.pl -species pmar01 -gff test_data/pmarinus4training.gff -fasta test_data/pmarinus4training.fa \
 -sout statsfile_out.pmar01 -fix_fasta 0
